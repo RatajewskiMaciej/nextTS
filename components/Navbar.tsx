cc133201896f82150523bcc75617a0086072e916
RatajewskiMaciej <ratajewskimaciej@wp.pl>
2023-10-12 15:58:16 +0200

    end of layout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,6 +10,10 @@ const Navbar = () => {
   <Link href="/" className="flex justify-center items-center">
     <Image src="logo.svg" alt="Car Hub Logo" width={118} height={18} className="object-contain"/>
   </Link>
+  <CustomButton
+  btnType="button"
+  title = "Sign In"
+   containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"/>
 </nav>
     </header>
   )
